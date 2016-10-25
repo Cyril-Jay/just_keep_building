92c22101fcbc1dd6295939e1c243d6a22db9139d
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-10-25 02:41:25 -0400

    stud basic calculation complete

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -1,2 +1,20 @@
 class Wall < ApplicationRecord
+
+  def initialize(length, height)
+    @length = length.to_i
+    @height = height.to_i
+  end
+
+  def square_footage
+    square_footage = @length * @height
+  end
+
+  def lenght_in_inches
+    lenght_in_inches = @length * 12
+  end
+
+  def studs
+    studs = 2 + (lenght_in_inches / 16).truncate
+  end
+
 end
