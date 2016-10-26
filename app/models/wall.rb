efa202ca40c4e6279a53e671c8b88dc01b42c50e
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-10-26 14:36:23 -0400

    drywall method set

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -4,15 +4,19 @@ class Wall < ApplicationRecord
   validates :length, presence: true
 
   def square_footage
-    @square_footage = length.to_i * height.to_i
+    length.to_i * height.to_i
   end
 
   def lenght_in_inches
-    lenght_in_inches = length.to_i * 12
+    length.to_i * 12
   end
 
   def studs
-    studs = 2 + (lenght_in_inches / 16.to_f).ceil
+    2 + (lenght_in_inches / 16.to_f).ceil
+  end
+
+  def drywall
+    (square_footage/32.to_f).ceil
   end
 
 end
