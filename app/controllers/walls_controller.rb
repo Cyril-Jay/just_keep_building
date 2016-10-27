c611961d85eb9665da144ac130b01a5c968917bf
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-10-27 14:59:56 -0400

    some fixed for hound

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -4,15 +4,15 @@ class WallsController < ApplicationController
   end
 
   def create
-  @wall = Wall.new(wall_params)
-  # @wall.user = current_user
-  if @wall.save
-    redirect_to wall_path(@wall)
-  else
-    # flash[:notice] = @wall.errors.full_messages.join(", ")
-    render :new
+    @wall = Wall.new(wall_params)
+    # @wall.user = current_user
+    if @wall.save
+      redirect_to wall_path(@wall)
+    else
+      # flash[:notice] = @wall.errors.full_messages.join(", ")
+      render :new
+    end
   end
-end
 
   def show
     @wall = Wall.find(params[:id])
@@ -20,7 +20,9 @@ end
 
   def edit
   end
+
   private
+
   def wall_params
     params.require(:wall).permit(:wall_name, :length, :height)
   end
