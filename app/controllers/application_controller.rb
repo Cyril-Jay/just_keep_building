128639864b3551435b544d808d48b40692aa20a1
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-10-28 09:33:44 -0400

    fixed edit and delete for walls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:index, :new]
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password])
+  end
+
   protect_from_forgery with: :exception
 end
--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -8,7 +8,9 @@ class WallsController < ApplicationController
   end
 
   def create
-    @wall = Wall.new(wall_params)
+    @wall = Wall.create(wall_params)
+    @wall.user_id = params[:user_id]
+    # @user = @wall.user
     @wall.user = current_user
     if @wall.save
       redirect_to wall_path(@wall)
@@ -23,6 +25,25 @@ class WallsController < ApplicationController
   end
 
   def edit
+    @wall = Wall.find(params[:id])
+  end
+
+  def update
+    @wall = Wall.find(params[:id])
+    if @wall.update_attributes(wall_params)
+      flash[:notice] = "Wall was succesfully edited."
+      redirect_to wall_path(@wall)
+    else
+      flash[:notice] = @wall.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  def destroy
+    @wall = Wall.find(params[:format])
+    @wall.destroy
+    flash[:notice] = "Wall removed"
+    redirect_to user_path(current_user)
   end
 
   private
