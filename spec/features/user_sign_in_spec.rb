3777ed2274270f44181ed6bbbf1800b62f7e860d
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-12-11 19:56:56 -0500

    first run at sign up

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 feature 'sign up' do
-  
+  context 'As an unauthenticated user, I want to sign up' do
+    scenario 'user signs up successfully' do
+
+      visit root_path
+      click_link 'Sign Up'
+      fill_in 'Email', with: 'build@gmail.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+      click_button 'Sign up'
+
+      expect(page).to have_content('Welcome back to Just Keep Building build@gmail.com')
+      expect(page).to have_content('Sign Out')
+    end
+  end
 end
