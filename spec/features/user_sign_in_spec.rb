2250398b385f8a3f6d9c4264460ab5aba3cc5232
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-12-18 20:27:15 -0500

    first test

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 feature 'sign in' do
+  context 'as an authenticated user, I want to be able to sign in' do
+  # let!(:user) { FactoryGirl.create(:user) }
+
+  scenario 'user signs in successfully' do
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    expect(page).to have_content('Sign Out')
+    expect(page).to have_content('Welcome back!')
+  end
+
   
 end
