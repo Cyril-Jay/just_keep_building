18c2691d77599b777eb7dec1a95988059362b224
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-12-26 11:32:06 -0500

    continuation of setup

--- spec/features/user_sees_list_of_walls_spec.rb
+++ spec/features/user_sees_list_of_walls_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
-feature 'non-regestered user sees as sign up or log in page' do
+feature 'non-rigestered user sees as sign up or log in page' do
 
   scenario 'a registered user sees a list of their walls' do
 
   end
 
-  scenario 'registered user will see logout in the drop down list' do
+  scenario 'registered user will see logout in the drop-down list' do
+
+  end
+
+  scenario 'non-registered user will not see a logout drop-down' do
     
   end
 
