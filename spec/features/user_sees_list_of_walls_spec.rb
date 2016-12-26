0e3e7fa229138e7037d4149b2b3c0856cd05d8a8
Cyril-Jay <Cyril_Jay@Hotmail.com>
2016-12-26 11:29:39 -0500

    setup for logout test.  will move at LD

--- spec/features/user_sees_list_of_walls_spec.rb
+++ spec/features/user_sees_list_of_walls_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 feature 'non-regestered user sees as sign up or log in page' do
 
   scenario 'a registered user sees a list of their walls' do
+
+  end
+
+  scenario 'registered user will see logout in the drop down list' do
     
   end
 
