57a8b195ad37fd249f3b97036eda58868ec22943
Cyril-Jay <Cyril_Jay@Hotmail.com>
2017-01-08 21:41:27 -0500

    shut off tests temp"

--- spec/features/user_sees_list_of_walls_spec.rb
+++ spec/features/user_sees_list_of_walls_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
-
-feature 'non-rigestered user sees as sign up or log in page' do
-
-  scenario 'a registered user sees a list of their walls' do
-
-  end
-
-  scenario 'registered user will see logout in the drop-down list' do
-
-  end
-
-  scenario 'non-registered user will not see a logout drop-down' do
-    
-  end
-
-end
+# 
+# feature 'non-rigestered user sees as sign up or log in page' do
+#
+#   scenario 'a registered user sees a list of their walls' do
+#
+#   end
+#
+#   scenario 'registered user will see logout in the drop-down list' do
+#
+#   end
+#
+#   scenario 'non-registered user will not see a logout drop-down' do
+#
+#   end
+#
+# end
