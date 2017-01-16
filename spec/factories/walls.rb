4189c3fbf0d4d46f0fd7e480d6bfabee6daa8c9b
Cyril-Jay <Cyril_Jay@Hotmail.com>
2017-01-15 23:22:50 -0500

    wall factory

--- spec/factories/walls.rb
+++ spec/factories/walls.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :wall do
-    sequence(:wall_name) { |n| "test wall#{n}" }
+    sequence(:wall_name) { |n| "master north#{n}" }
     sequence(:height) { |n| "#{n}" }
     sequence(:length) { |n| "#{n}" }
   end
