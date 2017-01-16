576cb2806c0752cf583f34c59b95a06f2cf8266d
Cyril-Jay <Cyril_Jay@Hotmail.com>
2017-01-15 23:19:01 -0500

    fixing factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "Mike#{n}s@building.com" }
+    sequence(:email) { |n| "Generic#{n}s@building.com" }
     sequence(:password) { |n| "thepassword#{n}" }
   end
 end
